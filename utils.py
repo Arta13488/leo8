8c1b07f3d93a0d847a7fc1c9e9be097da95ed2bc
Yermek Kapushev <yermek.kapushev@Yermeks-MacBook-Pro.local>
2018-09-25 13:29:11 +0300

    * Visualization of user acitivity

--- utils.py
+++ utils.py
@@ -2,11 +2,13 @@ from telethon import TelegramClient, sync
 from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 from tqdm import tqdm
 
+
 def start_client(api_id, api_hash, proxy_args=None):
     client = TelegramClient('session_name', api_id, api_hash,
                             proxy=proxy_args)
     client.start()
     return client
+
 
 def get_chat(client, chat_name):
     chat = None
@@ -17,7 +19,6 @@ def get_chat(client, chat_name):
     if chat is None:
         print("Can't find chat named '{}'".format(chat_name))
     return chat
-
 
 
 def get_chat_messages(client, chat):
@@ -48,16 +49,60 @@ def get_chat_messages(client, chat):
         
     return history
 
+
 def count_messages(history, username):
     info = history[username]['raw_messages']
     n_messages = len(info)
     return n_messages
+
+
+def count_words(history, username):
+    info = history[username]['raw_messages']
+    n_words = 0
+    for m in info:
+        if m.text is not None:
+            n_words += len(m.text.split())
+    return n_words
+
+
+def count_bytes(history, username):
+    info = history[username]['raw_messages']
+    n_bytes = 0
+    for m in info:
+        if m.text is not None:
+            n_bytes += len(m.text.encode('utf-8'))
+        if m.media is not None:
+            if hasattr(m.media, 'document'):
+                n_bytes += m.media.document.size
+            else:
+                print(m.media.stringify())
+                import pdb
+                pdb.set_trace()
+                for p in m.media.photo.sizes:
+                    n_bytes += p.size
+#                 photo = m.media.photo
+#                 n_bytes += _get_photo_size(photo)
+    return n_bytes
+
+
+def _get_photo_size(photo):
+    import collections
+    n_bytes = 0
+    import pdb
+    pdb.set_trace()
+    if isinstance(photo, collections.Iterable):
+        for p in photo:
+            n_bytes += p.size
+    else:
+        n_bytes += photo.size
+    return n_bytes
 
 def process_dates(history):
     import pandas as pd
     for user in history:
         history[user]['dates'] = pd.to_datetime([m.date for m in history[user]['raw_messages']])
 
+        
 def count_messages_after_date(history, date):
     counter = dict.fromkeys(history)
     for user in counter:
